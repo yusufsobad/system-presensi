8f92240157b53e51072091aa48b8b0c728ca1390
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2021-12-02 14:54:45 +0700

    Fix Bug

--- theme/default/template.php
+++ theme/default/template.php
@@ -136,16 +136,18 @@ abstract class metronic_template{
 								?>
 							</div>
 						</div>
-						<div class="modal-footer">
-							<?php
-								$button = $args['button'];
-								if(method_exists('metronic_template', $button)){
-									self::{$button}($args['status'],$idx);
-								}
-							?>
-						</div>
 					<?php endforeach; ?>
+						<button id="metronic-submit" type="submit" class="btn" style="display: none;"></button>
 				</form>
+
+				<div class="modal-footer">
+					<?php
+						$button = $args['button'];
+						if(method_exists('metronic_template', $button)){
+							self::{$button}($args['status'],$idx);
+						}
+					?>
+				</div>
 			</div>
 		<?php
 	}
@@ -179,11 +181,11 @@ abstract class metronic_template{
 		}
 		
 		?>
-		<button id="btn_<?php print($idx) ;?>" data-sobad="<?php print($args['link']) ;?>" data-load="<?php print($args['load']) ;?>" data-type="<?php print($type) ;?>" type="submit" class="btn blue" data-index="#<?php print($idx) ;?>" data-modal="<?php print($modal) ;?>" <?php print($status) ;?>>Save</button>
+		<button id="btn_<?php print($idx) ;?>" data-sobad="<?php print($args['link']) ;?>" data-load="<?php print($args['load']) ;?>" data-type="<?php print($type) ;?>" type="submit" class="btn blue" data-index="#<?php print($idx) ;?>" data-modal="<?php print($modal) ;?>" onclick="metronicSubmit_<?php print($idx) ;?>()" <?php print($status) ;?>>Save</button>
 		<button type="button" class="btn default" data-dismiss="modal">Cancel</button>
 
 		<script type="text/javascript">
-			//function metronicSubmit_<?php print($idx) ;?>(){
+			function metronicSubmit_<?php print($idx) ;?>(){
 				$("form#<?php print($idx) ;?>").validate({
 					errorElement: 'span', //default input error message container
 	                errorClass: 'help-block help-block-error', // default input error message class
@@ -201,8 +203,8 @@ abstract class metronic_template{
 				  	}
 				 });
 
-				//$("form<?php //print($index) ;?>>#metronic-submit").trigger("click");
-			//}
+				$("form#<?php print($idx) ;?>>#metronic-submit").trigger("click");
+			}
 		</script>
 		<?php
 	}
