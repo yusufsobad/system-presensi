d7bc21e9f11622a8339fc7f6df3a2d8d1440c2b9
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2022-04-18 10:29:56 +0700

    Fix bug

--- include/class_db/_class.php
+++ include/class_db/_class.php
@@ -201,10 +201,14 @@ abstract class _class{
 		// Check Temporary
 		if(isset($blueprint['temporary']) && self::$_temp){
 			$temp = $blueprint['temporary'];
-			$temp_table = "temp-" . $temp[$type]['temp'];
-			self::$_temp_table = $temp_table;
+			if(isset($temp[$type])){
+				$temp_table = "temp-" . $temp[$type]['temp'];
+				self::$_temp_table = $temp_table;
 
-			self::$_inner .= "LEFT JOIN `" . $table . "` ON `" . $temp_table . "`.reff_temp = `" . $table . "`.ID ";
+				self::$_inner .= "LEFT JOIN `" . $table . "` ON `" . $temp_table . "`.reff_temp = `" . $table . "`.ID ";
+			}else{
+				self::$_temp = false;
+			}
 		}
 	
 		if(isset($blueprint['detail'])){
